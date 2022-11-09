749a5b2d2f2201532d0f6536e6e06ce802c44f60
zaidmelhem <zaidomar2011426@gmail.com>
2022-11-09 19:22:20 +0200

    edit pages

--- lib/widgets/loadingWidget.dart
+++ lib/widgets/loadingWidget.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 class Loading extends StatelessWidget {
   const Loading({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home:Scaffold(
-        body: Center(
-          child: LoadingAnimationWidget.inkDrop(
-            color: Colors.black,
-            size: 100,
-          ),
-        )));
+    return MaterialApp(
+        home:Scaffold(
+            body: Center(
+              child: SpinKitFadingCircle(
+                itemBuilder: (BuildContext context, int index) {
+                  return DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: index.isEven ? Colors.black : Colors.white,
+                    ),
+                  );
+                },
+              ),
+            )));
   }
 }
